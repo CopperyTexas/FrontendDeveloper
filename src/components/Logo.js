63c7a9ea1d4a617f6f24e872f51cba8c508d29b4
CopperyTexas <dengleboff@yandex.ru>
2024-06-23 23:13:55 +0500

    nav

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,16 +1,22 @@
-import React from "react";
-import Link from "next/link";
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+import Link from 'next/link'
+import LogoGD from '../images/LogoGD.svg'
+
+const MotionLink = motion(Link)
 
 const Logo = () => {
-  return (
-    <div className="flex items-center justify-center mt-2">
-      <Link
-        href="/"
-        className="w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold"
-      >
-        GD
-      </Link>
-    </div>
-  );
-};
-export default Logo;
+	return (
+		<div className='flex items-center justify-center mt-2'>
+			<MotionLink
+				href='/'
+				whileHover={{
+					scale: 1.5,
+				}}
+			>
+				<Image src={LogoGD} alt='Logo' width={100} height={100} />
+			</MotionLink>
+		</div>
+	)
+}
+export default Logo
