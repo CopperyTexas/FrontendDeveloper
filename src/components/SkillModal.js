6f1b6f6c0ee7c5af456ead54b745d594cf3c725a
Denis Glebov <dengleboff@yandex.ru>
2024-07-09 17:32:20 +0500

    делаю модалку

--- src/components/SkillModal.js
+++ src/components/SkillModal.js
@@ -34,13 +34,7 @@ const SkillModal = ({ skill, onClose }) => {
 
 	return createPortal(
 		<div className='fixed inset-0 bg-dark bg-opacity-50 flex items-center justify-center z-50'>
-			<div className='bg-white p-6 rounded-lg max-w-md w-full text-center relative'>
-				<button
-					className='absolute top-2 right-2 text-gray-500 hover:text-gray-700'
-					onClick={onClose}
-				>
-					&times;
-				</button>
+			<div className='bg-light border-2 border-solid border-dark p-6 rounded-lg max-w-md w-full text-center relative'>
 				<div className='relative w-24 h-24 mb-4 mx-auto'>
 					<Image
 						src={skill.logo}
@@ -51,7 +45,7 @@ const SkillModal = ({ skill, onClose }) => {
 				</div>
 				<p className='mb-4'>{skill.description}</p>
 				<button
-					className='bg-blue-500 text-white py-2 px-4 rounded'
+					className='border-2 border-solid border-dark text-light py-2 px-4 rounded-lg font-medium bg-dark hover:bg-primaryDark hover:text-dark'
 					onClick={onClose}
 				>
 					Закрыть
