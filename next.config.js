c1f7d93afc864803e722f4659d39e255262c8f34
Denis Glebov <dengleboff@yandex.ru>
2024-07-23 16:33:29 +0500

    Public

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	output: 'export', // Настройка для экспорта проекта как статические файлы
 	webpack(config, options) {
 		config.module.rules.push({
 			test: /\.svg$/,
