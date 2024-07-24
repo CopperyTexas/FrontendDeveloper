4742b1c15bbb509f57b401ce76f8c7055a3da4bf
Denis Glebov <dengleboff@yandex.ru>
2024-07-24 11:27:06 +0500

    out

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-
+	output: 'export',
+	assetPrefix:
+		process.env.NODE_ENV === 'production' ? '/frontenddeveloper/' : '',
+	basePath: process.env.NODE_ENV === 'production' ? '/frontenddeveloper' : '',
+	trailingSlash: true,
 	webpack(config, options) {
 		config.module.rules.push({
 			test: /\.svg$/,
