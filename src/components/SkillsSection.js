e82c873d62548124671e89452cb0ee731ec013d9
CopperyTexas <dengleboff@yandex.ru>
2024-07-26 16:19:47 +0500

    разделяю все на компоненты

---
+++ src/components/SkillsSection.js
@@ -0,0 +1,92 @@
+import { motion, useInView } from 'framer-motion'
+import Image from 'next/image'
+import { useRef } from 'react'
+import Svg from '../images/code-type.svg'
+import AnimatedText from './AnimatedText'
+import SkillModal from './SkillModal'
+// Ваша часть кода с обновлением для анимации при прокрутке
+const SkillsSection = ({ skills, openModal, selectedSkill, closeModal }) => {
+	const ref = useRef(null)
+	const isInView = useInView(ref, { once: true })
+
+	return (
+		<div ref={ref} className='w-full grid grid-cols-2 grid-rows-[auto,1fr]'>
+			{/* Верхняя строка с компонентом AnimatedText */}
+			<div className='col-span-2'>
+				<AnimatedText
+					text='Каждый навык — шаг к успеху'
+					className='mt-32 !text-7xl !text-center xl:!text-5xl lg:!text-center lg:!text-5xl md:!text-5xl sm:!text-3xl'
+				/>
+			</div>
+			{/* Левый столбец с картинкой */}
+			<div className='col-span-1 flex justify-center items-center lg:col-span-2'>
+				<motion.div
+					initial={{ opacity: 0, x: -100 }} // начальное положение вне экрана справа
+					animate={isInView ? { opacity: 1, x: 0 } : {}} // конечное положение на экране
+					transition={{ delay: 0.3, duration: 0.6 }} // задержка и продолжительность анимации
+					className='w-full h-full lg:w-1/2'
+				>
+					<Svg />
+				</motion.div>
+			</div>
+			{/* Правый столбец со списком умений */}
+			<motion.div
+				className='col-span-1 flex justify-center items-start my-auto lg:col-span-2'
+				initial={{ opacity: 0, x: 100 }} // начальное положение вне экрана справа
+				animate={isInView ? { opacity: 1, x: 0 } : {}} // конечное положение на экране
+				transition={{ delay: 0.3, duration: 0.6 }} // задержка и продолжительность анимации
+			>
+				<motion.div
+					className='grid grid-cols-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-6 xl:grid-cols-4 2xl:grid-cols-4 gap-8 m-8'
+					initial='hidden'
+					whileInView='visible'
+					viewport={{ once: true }}
+					variants={{
+						hidden: { opacity: 0 },
+						visible: {
+							opacity: 1,
+							transition: {
+								staggerChildren: 0.3,
+							},
+						},
+					}}
+				>
+					{skills.map(skill => (
+						<motion.div
+							key={skill.id}
+							className='flex flex-col items-center cursor-pointer border-2 border-solid border-dark rounded-md p-2 dark:border-light px-10'
+							initial={{ opacity: 0, y: 20 }}
+							animate={{ opacity: 1, y: 0 }}
+							whileHover={{
+								scale: 0.95,
+								boxShadow: '0px 4px 15px rgba(146, 227, 169, 1)',
+							}}
+							whileTap={{ scale: 0.9 }}
+							onClick={() => openModal(skill)}
+						>
+							<div className='relative w-16 h-16 mb-2 xl:w-8 xl:h-8'>
+								<Image
+									src={skill.logo}
+									alt={skill.name}
+									fill
+									sizes='(max-width: 768px) 100vw,
+                          (max-width: 1200px) 50vw,
+                          33vw'
+									style={{ objectFit: 'contain' }}
+								/>
+							</div>
+							<h2 className='text-lg font-medium dark:text-light 2xl:text-sm xl:text-xs'>
+								{skill.name}
+							</h2>
+						</motion.div>
+					))}
+				</motion.div>
+				{selectedSkill && (
+					<SkillModal skill={selectedSkill} onClose={closeModal} />
+				)}
+			</motion.div>
+		</div>
+	)
+}
+
+export default SkillsSection
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,39 +1,22 @@
-import { motion, useInView, useMotionValue, useSpring } from 'framer-motion'
+import { motion } from 'framer-motion'
+import dynamic from 'next/dynamic'
 import Head from 'next/head'
 import Image from 'next/image'
-import { useEffect, useRef, useState } from 'react'
+import { useState } from 'react'
+import AnimatedNumbers from '../components/AnimatedNumbers'
 import AnimatedText from '../components/AnimatedText'
 import Layout from '../components/Layout'
-import SkillModal from '../components/SkillModal'
+import SkillsSection from '../components/SkillsSection'
 import TransitionEffect from '../components/TransitionEffect'
 import { AchievementColor, AchievementMono } from '../components/icons'
 import imageVariant from '../data/imageVariants'
 import skills from '../data/skills'
-import Svg from '../images/code-type.svg'
 import profilePic from '../images/photo_me.jpg'
-
-const AnimatedNumbers = ({ value }) => {
-	const ref = useRef(null)
-
-	const motionValue = useMotionValue(0)
-	const springValue = useSpring(motionValue, { duration: 5000 })
-	const isInView = useInView(ref, { once: true })
-
-	useEffect(() => {
-		if (isInView) {
-			motionValue.set(value)
-		}
-	}, [isInView, value, motionValue])
-
-	useEffect(() => {
-		springValue.on('change', latest => {
-			if (ref.current && latest.toFixed(0) <= value) {
-				ref.current.textContent = latest.toFixed(0)
-			}
-		})
-	}, [springValue, value])
-	return <span ref={ref}></span>
-}
+// Динамический импорт компонента SkillModal
+const SkillModal = dynamic(() => import('../components/SkillModal'), {
+	loading: () => <p>Loading...</p>,
+	ssr: false, // Отключение серверного рендеринга для компонента
+})
 
 const About = () => {
 	const [selectedSkill, setSelectedSkill] = useState(null)
@@ -51,6 +34,8 @@ const About = () => {
 			<Head>
 				<title>GlebovDenis | AboutPage</title>
 				<meta name='description' content='any description' />
+				{/* Добавление фавикона */}
+				<link rel='icon' href='/favicon.ico' />
 			</Head>
 			<TransitionEffect />
 			<main className='flex w-full flex-col items-center justify-center'>
@@ -72,7 +57,7 @@ const About = () => {
 								освоению новых инструментов и методологий.
 							</p>
 							<p className='mt-4 font-medium text-lg 2xl:text-sm xl:text-xs'>
-								<span className='p-1 pr-2 font-bold text-xl text-dark bg-primaryDark m-2 ml-0 rounded-lg 2xl:text-sm'>
+								<span className='p-1 pr-2 font-bold text-xl text-dark bg-primaryDark м-2 ml-0 rounded-lg 2xl:text-sm'>
 									Опыт и навыки
 								</span>
 								Хотя у меня еще нет коммерческого опыта, я компенсирую это своим
@@ -84,7 +69,7 @@ const About = () => {
 								обучающих материалов, книг и курсов.
 							</p>
 							<p className='mt-4 font-medium text-lg 2xl:text-sm xl:text-xs'>
-								<span className='p-1 pr-2 font-bold text-xl text-dark bg-primaryDark m-2 ml-0 rounded-lg 2xl:text-sm'>
+								<span className='p-1 pr-2 font-bold text-xl text-dark bg-primaryDark м-2 ml-0 rounded-lg 2xl:text-sm'>
 									Мой подход
 								</span>
 								Я верю в важность чистого и читаемого кода, а также в постоянное
@@ -94,7 +79,7 @@ const About = () => {
 								тестирование и документацию.
 							</p>
 							<p className='mt-4 font-medium text-lg 2xl:text-sm xl:text-xs'>
-								<span className='p-1 pr-2 font-bold text-xl text-dark bg-primaryDark m-2 ml-0 rounded-lg 2xl:text-sm'>
+								<span className='p-1 pr-2 font-bold text-xl text-dark bg-primaryDark м-2 ml-0 rounded-lg 2xl:text-sm'>
 									Цели
 								</span>
 								В ближайшем будущем я планирую углубить свои знания в области
@@ -107,7 +92,7 @@ const About = () => {
 								клиентам и пользователям.
 							</p>
 							<p className='mt-4 font-medium text-lg 2xl:text-sm xl:text-xs'>
-								<span className='p-1 pr-2 font-bold text-xl text-dark bg-primaryDark m-2 ml-0 rounded-lg 2xl:text-sm'>
+								<span className='p-1 pr-2 font-bold text-xl text-dark bg-primaryDark м-2 ml-0 rounded-lg 2xl:text-sm'>
 									Немного о себе
 								</span>
 								Помимо программирования, мои увлечения включают чтение
@@ -174,79 +159,12 @@ const About = () => {
 						</div>
 					</div>
 
-					<div className='w-full grid grid-cols-2 grid-rows-[auto,1fr]'>
-						{/* Верхняя строка с компонентом AnimatedText */}
-						<div className='col-span-2'>
-							<AnimatedText
-								text='Каждый навык — шаг к успеху'
-								className='mt-32 !text-7xl !text-center xl:!text-5xl lg:!text-center lg:!text-5xl md:!text-5xl sm:!text-3xl'
-							/>
-						</div>
-						{/* Левый столбец с картинкой */}
-						<div className='col-span-1 flex justify-center items-center lg:col-span-2'>
-							<motion.div
-								initial={{ opacity: 0, x: -100 }} // начальное положение вне экрана справа
-								animate={{ opacity: 1, x: 0 }} // конечное положение на экране
-								transition={{ delay: 3, duration: 1 }} // задержка и продолжительность анимации
-								className='w-full h-full lg:w-1/2'
-							>
-								<Svg />
-							</motion.div>
-						</div>
-						{/* Правый столбец со списком умений */}
-						<motion.div
-							className='col-span-1 flex justify-center items-start my-auto lg:col-span-2'
-							initial={{ opacity: 0, x: 100 }} // начальное положение вне экрана справа
-							animate={{ opacity: 1, x: 0 }} // конечное положение на экране
-							transition={{ delay: 3, duration: 1 }} // задержка и продолжительность анимации
-						>
-							<motion.div
-								className='grid grid-cols-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-6 xl:grid-cols-4 2xl:grid-cols-4 gap-8 m-8'
-								initial='hidden'
-								whileInView='visible'
-								viewport={{ once: true }}
-								variants={{
-									hidden: { opacity: 0 },
-									visible: {
-										opacity: 1,
-										transition: {
-											staggerChildren: 0.3,
-										},
-									},
-								}}
-							>
-								{skills.map(skill => (
-									<motion.div
-										key={skill.id}
-										className='flex flex-col items-center cursor-pointer border-2 border-solid border-dark rounded-md p-2 dark:border-light px-10'
-										initial={{ opacity: 0, y: 20 }}
-										animate={{ opacity: 1, y: 0 }}
-										whileHover={{
-											scale: 0.95,
-											boxShadow: '0px 4px 15px rgba(146, 227, 169, 1)',
-										}}
-										whileTap={{ scale: 0.9 }}
-										onClick={() => openModal(skill)}
-									>
-										<div className='relative w-16 h-16 mb-2 xl:w-8 xl:h-8'>
-											<Image
-												src={skill.logo}
-												alt={skill.name}
-												layout='fill'
-												objectFit='contain'
-											/>
-										</div>
-										<h2 className='text-lg font-medium dark:text-light 2xl:text-sm xl:text-xs'>
-											{skill.name}
-										</h2>
-									</motion.div>
-								))}
-							</motion.div>
-							{selectedSkill && (
-								<SkillModal skill={selectedSkill} onClose={closeModal} />
-							)}
-						</motion.div>
-					</div>
+					<SkillsSection
+						skills={skills}
+						openModal={openModal}
+						selectedSkill={selectedSkill}
+						closeModal={closeModal}
+					/>
 				</Layout>
 			</main>
 		</>
